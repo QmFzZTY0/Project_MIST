efbf2b5bca17ef181aee93b9e1e1e58ae7bc0984
Delta <zzy19981225@gmail.com>
2019-04-30 06:11:50 +0800

    fix bug

--- temp_control/get_temp.py
+++
@@ -1,9 +0,0 @@
-#-*- coding: utf-8 -*-
-#this is a module for temp Monitor program
-
-def get_temp():
-	file = open("/sys/class/thermal/thermal_zone0/temp")   
-	temp = float(file.read()) / 1000  
-	file.close()  
-	t = ("temp : %.1f" %temp)[6:] 
-	return t
