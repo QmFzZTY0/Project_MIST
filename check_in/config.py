ad26defd3c893ca61ea3c573a8de508e41d9ea67
Delta <zzy19981225@gmail.com>
2019-04-30 17:56:01 +0800

    添加了配置文件

---
+++ check_in/config.py
@@ -0,0 +1,10 @@
+def config(inf):
+    cookie = ''#目标cookie
+    referer = ''#登录地址
+    target = ''#访问目标
+    if inf == 'cookie':
+        return cookie
+    elif inf == 'referer':
+        return referer
+    elif inf == 'target':
+        return target
--- check_in/main.py
+++ check_in/main.py
@@ -1,22 +1,10 @@
 import requests
+from config import config
 
-try:
-    f = open('cookie.txt','r')
-
-except IOError:
-    print('file 404')
-
-else:
-    for line in f.readlines():
-        cookie = line.strip('\n')
-    headers={
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
-            'Referer':'https://steamcn.com/member.php?mod=logging&action=login',
-            'Cookie':cookie
+headers={
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+        'Referer':config('referer'),
+        'Cookie':config('cookie')
         }
 
-    res=requests.get("https://steamcn.com/",headers=headers)
-
-finally:
-    f.close()
-
+res=requests.get(config('target'),headers=headers)
