67c51ad9151014c4b1459cc7e597c40ca194adfa
QmFzZTY0 <zzy19981225@gmail.com>
2019-05-27 15:59:39 +0800

    规范了配置文件，添加了自动生成配置文件的功能

--- sendmail/main.py
+++ sendmail/main.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
+#!/usr/lib/python2.7
+# -*- coding:utf8 -*-
 
 import smtplib
 import string
@@ -7,9 +7,24 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 import urllib
 import urllib2
-from config import config
+from configparser import ConfigParser
+import io
 
-
+config = ConfigParser()
+if len(config.read('config.ini')) == 0:
+    print'配置文件为空,准备创建配置文件'
+    config.add_section('main')  # 添加 section
+    config.set('main', 'send', 'null')
+    config.set('main', 'receive', 'null')
+    config.set('main','address','smtp.163.com')
+    config.set('main', 'port', '25')
+    config.set('main', 'send_password', 'null')
+    config.set('main', 'ip_txt', 'ip.txt')
+    with io.open('config.ini', 'w', encoding='utf-8') as file:
+        config.write(file)
+    print'配置文件创建完成,请更改配置文件后在次运行'
+    exit(0)
+config.read('config.ini')
 user_agent = 'Mozilla/4.0 (compatible; MSIE 10.0; Windows NT)'
 headers = {}
 headers['User-Agent'] = user_agent
@@ -29,9 +44,12 @@ while True:
 if f.read()==ip:
     print 'pass'
     f.close()
+elif config.get('main','send')=='null':
+    print'配置文件未修改，停止中'
+    exit(0)
 else:
-    fromaddr = config('send')  # 填写你的发信邮箱，我选用的是163邮箱
-    toaddr = config('receive')   # 填写你的收信地址，接收树莓派的公网IP地址
+    fromaddr = config.get('main','send')  # 填写你的发信邮箱，我选用的是163邮箱
+    toaddr = config.get('main','receive')   # 填写你的收信地址，接收树莓派的公网IP地址
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
@@ -40,9 +58,9 @@ else:
     body = 'rasperberry IP is ' + ip   # 邮件内容，同标题（偷懒）
     msg.attach(MIMEText(body, 'plain'))
     
-    server = smtplib.SMTP(config('address'), config('port'))   # 填写163邮箱的发信服务器地址
+    server = smtplib.SMTP(config.get('main','address'), config.get('main','port'))   # 填写163邮箱的发信服务器地址
     server.starttls()
-    server.login(fromaddr, config('send_password'))   # xxx代表你的邮件登录密码
+    server.login(fromaddr, config.get('main','send_password'))   # xxx代表你的邮件登录密码
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text) # 开始发邮件
     print u"send ok"  # 发送成功提示
