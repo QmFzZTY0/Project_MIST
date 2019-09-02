a64b6dc1969e82ad3ec584c1680bdd9b1c1e8858
QmFzZTY0 <zzy19981225@gmail.com>
2019-09-03 02:57:01 +0800

    添加注释

--- trackers_best_ip/main.py
+++ trackers_best_ip/main.py
@@ -5,21 +5,25 @@ import requests
 import os
 
 l = [] #提前创建空列表存放内容
-website ='https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt'
-def get_best(website):
+
+website = 'https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt'
+traget = '/home/pi/.config/aria2/aria2.config'
+
+def get_best(website):#获取数据并且进行处理
     res = requests.get(website)
     res = (res.text.replace('\n\n',',')).strip(',')
     return res
-with open('/home/pi/.config/aria2/aria2.config','r')as f:
+with open(traget,'r')as f:#读取config
         for i in f:
             if 'bt-tracker=' not in i:
                 l.append(i)
             elif 'bt-tracker=' in i:
                 l.append('bt-tracker={}'.format(get_best(website)))
 #print (l)
-with open('/home/pi/.config/aria2/aria2.config','w')as f:
+with open(traget,'w')as f:#修改config
     for i in l:
         f.write(i)
 
-os.system('sudo bash /home/pi/code/trackers_best_ip/reboot.sh')
+os.system('sudo bash /home/pi/code/trackers_best_ip/reboot.sh')#让aria2重启
+
 print ('ok')
